3919a95daeff3de3fd901271ae9ac0e731ae0ba0
uzair-bmj <uzairbmj@gmail.com>
2024-03-06 22:58:34 +0500

    api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import dbconnection from "./db/index.js"
+import { app } from "./App.js";
 
 
 config({
@@ -7,4 +8,12 @@ config({
 });
 
 dbconnection()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err=>{
+    console.log("database connected failes !! ", err)
+}))
 
